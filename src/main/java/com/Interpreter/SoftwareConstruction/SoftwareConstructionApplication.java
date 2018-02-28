916d8ecba3fe50cd5cf45a8008f52c010a619302
Ioana Mirică <ioana.mirica@student.uva.nl>
2018-02-28 13:59:25 +0100

    SpringBoot

---
+++ src/main/java/com/Interpreter/SoftwareConstruction/SoftwareConstructionApplication.java
@@ -0,0 +1,12 @@
+package com.Interpreter.SoftwareConstruction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareConstructionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareConstructionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Interpreter/SoftwareConstruction/SoftwareConstructionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Interpreter.SoftwareConstruction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoftwareConstructionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
